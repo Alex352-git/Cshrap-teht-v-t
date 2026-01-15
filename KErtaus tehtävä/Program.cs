bf8c00cc65357cb40ade3ba5d4a67e2586bd76f0
Alex352-git <s2401168@edu.bc.fi>
2026-01-15 12:46:14 +0200

    Tehtävät
    done

---
+++ KErtaus tehtävä/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace KErtaus_tehtävä
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ritari_hp = 15;
+            int orkki_hp = 15;
+            Random random = new Random();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello, Brave Adventurer. Time to kill orc!");
+            Console.ResetColor();
+            while (true)
+            {
+                int orkkidamage = random.Next(1, 6);
+                int ritaridamage = random.Next(1, 6);
+                int oneshot = random.Next(1, 100);
+                Console.WriteLine($"Ritarin HP: {ritari_hp}/15");
+                Console.WriteLine($"Örkin HP: {orkki_hp}/15");
+                Console.WriteLine("Anna komento: 1 hyökkää, 2 puolusta");
+                string vastaus = Console.ReadLine();
+                if (vastaus == "1")
+                {
+                    orkki_hp -= ritaridamage;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Ritari tekee {ritaridamage} vahinkoo");
+                    Console.ResetColor();
+                    if (oneshot == 1)
+                    {
+                        orkki_hp -= 1000;
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine("CRITICAL HIT!!!");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                        Console.WriteLine("SURMASIT ÖRKIN");
+                        break;
+                    }
+
+
+
+                }
+                else if (vastaus == "2")
+                {
+                    orkkidamage -= 2;
+                    if (orkkidamage <= 0)
+                    {
+                        orkkidamage = 1;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Ritari puolustaa {orkkidamage} vahinkoo");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("En ymmärrä!");
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Örkki hyökkää pelaaja ottaa {orkkidamage} vahinkoo!");
+                Console.ResetColor();
+                ritari_hp -= orkkidamage;
+
+
+                if (ritari_hp <= 0 && orkki_hp <= 0)
+                {
+                    Console.WriteLine("Eeppinen tappelu molemmat kuolitte");
+                    break;
+                }
+
+                if (orkki_hp <= 0)
+                {
+                    Console.WriteLine("tapoit Örkin hanen lapset itkee");
+                    break;
+                }
+                if (ritari_hp <= 0)
+                {
+                    Console.WriteLine("Kuolit oot paska");
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Raylibtesti/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+namespace Raylibtesti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "Raylib");
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Gold);
+                Raylib.DrawCircle(
+                    Raylib.GetScreenWidth() / 2,
+                    Raylib.GetScreenHeight() / 2,
+                    1 + (float)Raylib.GetTime() * 4,
+                    Color.Black);
+                Raylib.EndDrawing();
+
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
