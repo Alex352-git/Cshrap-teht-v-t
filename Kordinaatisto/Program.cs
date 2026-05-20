e735ca272c5d753bfdda47e37e7927b71db55fdf
Alex352-git <s2401168@edu.bc.fi>
2026-05-21 01:15:51 +0300

    väritetyt shidit ja kordinaatisto
    dsadas

---
+++ Kordinaatisto/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Koordinaatisto
+{
+    public struct Koordinaatti
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Koordinaatti(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool OnkoVieressa(Koordinaatti toinen)
+        {
+            int etaisyysX = Math.Abs(this.X - toinen.X);
+            int etaisyysY = Math.Abs(this.Y - toinen.Y);
+
+            return etaisyysX <= 1 && etaisyysY <= 1 && !(etaisyysX == 0 && etaisyysY == 0);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Koordinaatti nollaKoordinaatti = new Koordinaatti(0, 0);
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    Koordinaatti testattava = new Koordinaatti(x, y);
+
+                    if (testattava.X == nollaKoordinaatti.X && testattava.Y == nollaKoordinaatti.Y)
+                    {
+                        Console.WriteLine($"Annettu koordinaatti {testattava.X},{testattava.Y} on koordinaatissa {nollaKoordinaatti.X},{nollaKoordinaatti.Y}.");
+                    }
+                    else if (testattava.OnkoVieressa(nollaKoordinaatti))
+                    {
+                        Console.WriteLine($"Annettu koordinaatti {testattava.X},{testattava.Y} on koordinaatin {nollaKoordinaatti.X},{nollaKoordinaatti.Y} vieressä.");
+                    }
+                }
+            }
+        }
+    }
+}
--- Reppu/Program.cs
+++ Reppu/Program.cs
@@ -52,36 +52,61 @@ namespace Reppu
         public override string ToString() { return "Miekka"; }
     }
 
+    public class Kirves : Tavara
+    {
+        public Kirves() : base(4, 2) { }
+        public override string ToString() { return "Kirves"; }
+    }
+
+    public class VaritettyTavara<T>
+    {
+        public T Esine { get; }
+        public ConsoleColor Vari { get; }
+
+        public VaritettyTavara(T esine, ConsoleColor vari)
+        {
+            Esine = esine;
+            Vari = vari;
+        }
+
+        public void NaytaTavara()
+        {
+            ConsoleColor vanhaVari = Console.ForegroundColor;
+            Console.ForegroundColor = Vari;
+            Console.WriteLine(Esine.ToString());
+            Console.ForegroundColor = vanhaVari;
+        }
+    }
+
     public class Reppu
     {
         private int maxTavarat;
         private double maxPaino;
         private double maxTilavuus;
-        private List<Tavara> tavarat;
+        private List<VaritettyTavara<Tavara>> tavarat;
 
         public Reppu(int maxTavarat, double maxPaino, double maxTilavuus)
         {
             this.maxTavarat = maxTavarat;
             this.maxPaino = maxPaino;
             this.maxTilavuus = maxTilavuus;
-            this.tavarat = new List<Tavara>();
+            this.tavarat = new List<VaritettyTavara<Tavara>>();
         }
 
         public int NykyinenTavaraMaara => tavarat.Count;
-        public double NykyinenPaino => tavarat.Sum(t => t.Paino);
-        public double NykyinenTilavuus => tavarat.Sum(t => t.Tilavuus);
+        public double NykyinenPaino => tavarat.Sum(t => t.Esine.Paino);
+        public double NykyinenTilavuus => tavarat.Sum(t => t.Esine.Tilavuus);
 
-        public bool Lisää(Tavara tavara)
+        public bool Lisää(VaritettyTavara<Tavara> tavara)
         {
             if (NykyinenTavaraMaara + 1 > maxTavarat) return false;
-            if (NykyinenPaino + tavara.Paino > maxPaino) return false;
-            if (NykyinenTilavuus + tavara.Tilavuus > maxTilavuus) return false;
+            if (NykyinenPaino + tavara.Esine.Paino > maxPaino) return false;
+            if (NykyinenTilavuus + tavara.Esine.Tilavuus > maxTilavuus) return false;
 
             tavarat.Add(tavara);
             return true;
         }
 
-      
         public override string ToString()
         {
             if (tavarat.Count == 0)
@@ -89,11 +114,9 @@ namespace Reppu
                 return "Reppu on tyhjä.";
             }
 
-          
-            return "Repussa on seuraavat tavarat: " + string.Join(", ", tavarat);
+            return "Repussa on seuraavat tavarat: " + string.Join(", ", tavarat.Select(t => t.Esine.ToString()));
         }
 
-        
         public string KapasiteettiRaportti()
         {
             return $"Repussa on tällä hetkellä {NykyinenTavaraMaara}/{maxTavarat} tavaraa, {NykyinenPaino}/{maxPaino} painoa, and {NykyinenTilavuus}/{maxTilavuus} tilavuus.";
@@ -102,15 +125,29 @@ namespace Reppu
 
     class Program
     {
+        public static void NaytaTavara<T>(VaritettyTavara<T> varitettyTavara)
+        {
+            varitettyTavara.NaytaTavara();
+        }
+
         static void Main(string[] args)
         {
+            VaritettyTavara<Miekka> esimerkkimiekka = new VaritettyTavara<Miekka>(new Miekka(), ConsoleColor.Blue);
+            NaytaTavara(esimerkkimiekka);
+
+            VaritettyTavara<Jousi> esimerkkijousi = new VaritettyTavara<Jousi>(new Jousi(), ConsoleColor.Red);
+            esimerkkijousi.NaytaTavara();
+
+            VaritettyTavara<Kirves> esimerkkikirves = new VaritettyTavara<Kirves>(new Kirves(), ConsoleColor.Green);
+            esimerkkikirves.NaytaTavara();
+
+            Console.WriteLine();
+
             Reppu reppu = new Reppu(10, 30, 20);
 
             while (true)
             {
-                
                 Console.WriteLine(reppu.KapasiteettiRaportti());
-                
                 Console.WriteLine(reppu.ToString());
 
                 Console.WriteLine("Mitä haluat lisätä?");
@@ -122,16 +159,16 @@ namespace Reppu
                 Console.WriteLine("6 - Miekka");
 
                 string valinta = Console.ReadLine();
-                Tavara uusiTavara = null;
+                VaritettyTavara<Tavara> uusiTavara = null;
 
                 switch (valinta)
                 {
-                    case "1": uusiTavara = new Nuoli(); break;
-                    case "2": uusiTavara = new Jousi(); break;
-                    case "3": uusiTavara = new Köysi(); break;
-                    case "4": uusiTavara = new Vesi(); break;
-                    case "5": uusiTavara = new Ruoka(); break;
-                    case "6": uusiTavara = new Miekka(); break;
+                    case "1": uusiTavara = new VaritettyTavara<Tavara>(new Nuoli(), ConsoleColor.White); break;
+                    case "2": uusiTavara = new VaritettyTavara<Tavara>(new Jousi(), ConsoleColor.Red); break;
+                    case "3": uusiTavara = new VaritettyTavara<Tavara>(new Köysi(), ConsoleColor.Yellow); break;
+                    case "4": uusiTavara = new VaritettyTavara<Tavara>(new Vesi(), ConsoleColor.Blue); break;
+                    case "5": uusiTavara = new VaritettyTavara<Tavara>(new Ruoka(), ConsoleColor.Green); break;
+                    case "6": uusiTavara = new VaritettyTavara<Tavara>(new Miekka(), ConsoleColor.Cyan); break;
                     default: continue;
                 }
 
